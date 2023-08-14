fdbd7fcda62702afbe48db25f22f519003575261
NIRMAL CHHETRI <105311660+nirmalkchhetri07@users.noreply.github.com>
2023-08-15 00:48:24 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+// var d = new Date();
+// var time = d.getHours();
+// if(time<10){
+//     document.write('Good Morning')
+// }
+// else if(time>=10 & time<16){
+//     document.write('Good day')
+// }
+// else(time>=16 & time < 16){
+//     document.write('good night')
+// }
+// document.getElementById('demo').innerHTML= d;
